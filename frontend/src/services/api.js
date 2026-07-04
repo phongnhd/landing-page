6d38fda60d0a8c450de4cd33e69168a072029369
Phongnhd <phongnhd2605@gmail.com>
2026-07-04 11:49:09 +0700

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
